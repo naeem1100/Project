c6f09fbd3966005df7b5285f9d9c0715cb81bfaa
naeem1100 <naeemkhan09807@gmail.com>
2023-04-05 08:28:53 +0500

    My project

---
+++ assets/js/body_them.js
@@ -0,0 +1,139 @@
+function changeColor(){
+    document.getElementById("main").style="display: flex; width: 50%; justify-content: center; margin-left: 25%; margin-top: 10%";
+    document.getElementById("no").style="display: none";
+    
+    }
+
+    function prev(){
+        document.getElementById("pre").style="display: none";
+     }
+
+    function red(){
+    document.getElementById("id1").style="background-color: red";
+    document.getElementById("blue").style="display: none";
+    document.getElementById("green").style="display: none";
+    document.getElementById("yellow").style="display: none";
+    document.getElementById("skyblue").style="display: none";
+    document.getElementById("cyan").style="display: none";
+    document.getElementById("purple").style="display: none"; 
+    document.getElementById("pink").style="display: none"; 
+    document.getElementById("brown").style="display: none"; 
+    document.getElementById("royalblue").style="display: none"; 
+    }
+
+    function blue(){
+    document.getElementById("id1").style="background-color: blue";
+    document.getElementById("red").style="display: none";
+    document.getElementById("green").style="display: none";
+    document.getElementById("yellow").style="display: none";
+    document.getElementById("skyblue").style="display: none";
+    document.getElementById("cyan").style="display: none";
+    document.getElementById("purple").style="display: none"; 
+    document.getElementById("pink").style="display: none"; 
+    document.getElementById("brown").style="display: none"; 
+    document.getElementById("royalblue").style="display: none"; 
+    }
+
+    function red(){
+    document.getElementById("id1").style="background-color: green";
+    document.getElementById("blue").style="display: none";
+    document.getElementById("red").style="display: none";
+    document.getElementById("yellow").style="display: none";
+    document.getElementById("skyblue").style="display: none";
+    document.getElementById("cyan").style="display: none";
+    document.getElementById("purple").style="display: none"; 
+    document.getElementById("pink").style="display: none"; 
+    document.getElementById("brown").style="display: none"; 
+    document.getElementById("royalblue").style="display: none"; 
+    }
+
+    function yellow(){
+    document.getElementById("id1").style="background-color: yellow";
+    document.getElementById("blue").style="display: none";
+    document.getElementById("green").style="display: none";
+    document.getElementById("red").style="display: none";
+    document.getElementById("skyblue").style="display: none";
+    document.getElementById("cyan").style="display: none";
+    document.getElementById("purple").style="display: none"; 
+    document.getElementById("pink").style="display: none"; 
+    document.getElementById("brown").style="display: none"; 
+    document.getElementById("royalblue").style="display: none"; 
+    }
+    
+    function skyblue(){
+    document.getElementById("id1").style="background-color: skyblue";
+    document.getElementById("blue").style="display: none";
+    document.getElementById("green").style="display: none";
+    document.getElementById("yellow").style="display: none";
+    document.getElementById("red").style="display: none";
+    document.getElementById("cyan").style="display: none";
+    document.getElementById("purple").style="display: none"; 
+    document.getElementById("pink").style="display: none"; 
+    document.getElementById("brown").style="display: none"; 
+    document.getElementById("royalblue").style="display: none"; 
+    }
+
+    function cyan(){
+    document.getElementById("id1").style="background-color: cyan";
+    document.getElementById("blue").style="display: none";
+    document.getElementById("green").style="display: none";
+    document.getElementById("yellow").style="display: none";
+    document.getElementById("skyblue").style="display: none";
+    document.getElementById("red").style="display: none";
+    document.getElementById("purple").style="display: none"; 
+    document.getElementById("pink").style="display: none"; 
+    document.getElementById("brown").style="display: none"; 
+    document.getElementById("royalblue").style="display: none"; 
+    }
+
+    function purple(){
+    document.getElementById("id1").style="background-color: purple";
+    document.getElementById("blue").style="display: none";
+    document.getElementById("green").style="display: none";
+    document.getElementById("yellow").style="display: none";
+    document.getElementById("skyblue").style="display: none";
+    document.getElementById("cyan").style="display: none";
+    document.getElementById("red").style="display: none"; 
+    document.getElementById("pink").style="display: none"; 
+    document.getElementById("brown").style="display: none"; 
+    document.getElementById("royalblue").style="display: none"; 
+    }
+
+    function pink(){
+    document.getElementById("id1").style="background-color: pink";
+    document.getElementById("blue").style="display: none";
+    document.getElementById("green").style="display: none";
+    document.getElementById("yellow").style="display: none";
+    document.getElementById("skyblue").style="display: none";
+    document.getElementById("cyan").style="display: none";
+    document.getElementById("purple").style="display: none"; 
+    document.getElementById("red").style="display: none"; 
+    document.getElementById("brown").style="display: none"; 
+    document.getElementById("royalblue").style="display: none"; 
+    }
+
+    function brown(){
+    document.getElementById("id1").style="background-color: brown";
+    document.getElementById("blue").style="display: none";
+    document.getElementById("green").style="display: none";
+    document.getElementById("yellow").style="display: none";
+    document.getElementById("skyblue").style="display: none";
+    document.getElementById("cyan").style="display: none";
+    document.getElementById("purple").style="display: none"; 
+    document.getElementById("pink").style="display: none"; 
+    document.getElementById("red").style="display: none"; 
+    document.getElementById("royalblue").style="display: none"; 
+    }
+
+    function royalblue(){
+    document.getElementById("id1").style="background-color: royalblue";
+    document.getElementById("blue").style="display: none";
+    document.getElementById("green").style="display: none";
+    document.getElementById("yellow").style="display: none";
+    document.getElementById("skyblue").style="display: none";
+    document.getElementById("cyan").style="display: none";
+    document.getElementById("purple").style="display: none"; 
+    document.getElementById("pink").style="display: none"; 
+    document.getElementById("brown").style="display: none"; 
+    document.getElementById("red").style="display: none"; 
+    }
---
+++ assets/js/jsfile.js
@@ -0,0 +1,165 @@
+
+    function brand1(){
+        var productOne = document.getElementById("brand1").selectedIndex;
+
+        if(productOne == 1){
+            document.getElementById("img1").src="../assets/images/dress1.jpeg";
+            document.getElementById("price").innerHTML = '$45,00 &nbsp <s>6,000</s>' ;
+            document.getElementById("content").innerHTML = 'No Cuts Given Bandage beuti Midi Dress';
+        }
+
+        if(productOne == 2){
+            document.getElementById("img1").src="../assets/images/dress11.jpeg";
+            document.getElementById("price").innerHTML = '$3,000 &nbsp <s>5,000</s>' ;
+            document.getElementById("content").innerHTML = 'No Cuts Given Bandage beuti Midi Dress';
+        }
+
+        if(productOne == 3){
+            document.getElementById("img1").src="../assets/images/dress9.png";
+            document.getElementById("price").innerHTML = '$5,000 &nbsp <s>7,000</s>' ;
+            document.getElementById("content").innerHTML = 'No Cuts Given Bandage beuti Midi Dress';
+        }
+        if(productOne == 4){
+            document.getElementById("img1").src="../assets/images/n (1).jpeg";
+            document.getElementById("price").innerHTML = '$45,00 &nbsp <s>6,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 5){
+            document.getElementById("img1").src="../assets/images/n (1).jpg";
+            document.getElementById("price").innerHTML = '$3,000 &nbsp <s>5,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 6){
+            document.getElementById("img1").src="../assets/images/Reiss.jpg";
+            document.getElementById("price").innerHTML = '$3,000 &nbsp <s>5,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 7){
+            document.getElementById("img1").src="../assets/images/n (1).webp";
+            document.getElementById("price").innerHTML = '$25,00 &nbsp <s>4,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 8){
+            document.getElementById("img1").src="../assets/images/dress15.png";
+            document.getElementById("price").innerHTML = '$45,00 &nbsp <s>6,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 9){
+            document.getElementById("img1").src="../assets/images/riess3.jpg";
+            document.getElementById("price").innerHTML = '$25,00 &nbsp <s>4,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 10){
+            document.getElementById("img1").src="../assets/images/n (2).webp";
+            document.getElementById("price").innerHTML = '$3,000 &nbsp <s>5,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 11){
+            document.getElementById("img1").src="../assets/images/authum (1).jpg";
+            document.getElementById("price").innerHTML = '$5,000 &nbsp <s>7,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 12){
+            document.getElementById("img1").src="../assets/images/wedding.webp";
+            document.getElementById("price").innerHTML = '$45,00 &nbsp <s>6,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 13){
+            document.getElementById("img1").src="../assets/images/wedding4.webp";
+            document.getElementById("price").innerHTML = '$5,00 &nbsp <s>7,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 14){
+            document.getElementById("img1").src="../assets/images/wedding5.webp";
+            document.getElementById("price").innerHTML = '$6,00 &nbsp <s>4,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 15){
+            document.getElementById("img1").src="../assets/images/wedding8.webp";
+            document.getElementById("price").innerHTML = '$7,000 &nbsp <s>9,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+    }
+    // *********** Start brand2 ************ \\
+    function brand2(){
+        var productOne = document.getElementById("brand2").selectedIndex;
+
+        if(productOne == 1){
+            document.getElementById("img2").src="../assets/images/dress1.jpeg";
+            document.getElementById("price").innerHTML = '$45,00 &nbsp <s>6,000</s>' ;
+            document.getElementById("content").innerHTML = 'No Cuts Given Bandage beuti Midi Dress';
+        }
+
+        if(productOne == 2){
+            document.getElementById("img2").src="../assets/images/dress11.jpeg";
+            document.getElementById("price").innerHTML = '$3,000 &nbsp <s>5,000</s>' ;
+            document.getElementById("content").innerHTML = 'No Cuts Given Bandage beuti Midi Dress';
+        }
+
+        if(productOne == 3){
+            document.getElementById("img2").src="../assets/images/dress9.png";
+            document.getElementById("price").innerHTML = '$5,000 &nbsp <s>7,000</s>' ;
+            document.getElementById("content").innerHTML = 'No Cuts Given Bandage beuti Midi Dress';
+        }
+        if(productOne == 4){
+            document.getElementById("img2").src="../assets/images/n (1).jpeg";
+            document.getElementById("price").innerHTML = '$45,00 &nbsp <s>6,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 5){
+            document.getElementById("img2").src="../assets/images/n (1).jpg";
+            document.getElementById("price").innerHTML = '$3,000 &nbsp <s>5,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 6){
+            document.getElementById("img2").src="../assets/images/Reiss.jpg";
+            document.getElementById("price").innerHTML = '$3,000 &nbsp <s>5,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 7){
+            document.getElementById("img2").src="../assets/images/n (1).webp";
+            document.getElementById("price").innerHTML = '$25,00 &nbsp <s>4,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 8){
+            document.getElementById("img2").src="../assets/images/dress15.png";
+            document.getElementById("price").innerHTML = '$45,00 &nbsp <s>6,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 9){
+            document.getElementById("img2").src="../assets/images/riess3.jpg";
+            document.getElementById("price").innerHTML = '$25,00 &nbsp <s>4,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 10){
+            document.getElementById("img2").src="../assets/images/n (2).webp";
+            document.getElementById("price").innerHTML = '$3,000 &nbsp <s>5,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 11){
+            document.getElementById("img2").src="../assets/images/authum (1).jpg";
+            document.getElementById("price").innerHTML = '$5,000 &nbsp <s>7,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 12){
+            document.getElementById("img2").src="../assets/images/wedding.webp";
+            document.getElementById("price").innerHTML = '$45,00 &nbsp <s>6,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 13){
+            document.getElementById("img2").src="../assets/images/wedding4.webp";
+            document.getElementById("price").innerHTML = '$5,00 &nbsp <s>7,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 14){
+            document.getElementById("img2").src="../assets/images/wedding5.webp";
+            document.getElementById("price").innerHTML = '$6,00 &nbsp <s>4,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+        if(productOne == 15){
+            document.getElementById("img2").src="../assets/images/wedding8.webp";
+            document.getElementById("price").innerHTML = '$7,000 &nbsp <s>9,000</s>' ;
+            document.getElementById("content").innerHTML = 'Rayna Denim Midi beauti Dress';
+        }
+    }
+    // *********** End brand2   ************ \\
